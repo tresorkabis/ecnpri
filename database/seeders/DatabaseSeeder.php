4544f7225e993dec6b924b7fa0f35828d974dfe2
tresor <tresorkabis@gmail.com>
2026-05-11 02:11:37 +0100

    maj

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,9 +17,14 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
+        // Commented out to prevent "Call to a member function name() on null" error
+        // as CnpriSeeder already creates an admin user and this factory call
+        // seems to be causing issues with Faker initialization in this context.
+        /*
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+        */
     }
 }
